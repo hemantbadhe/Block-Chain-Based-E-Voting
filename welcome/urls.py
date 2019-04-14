9d5253e09d02d2562fc8d01fb0ec0bd2beaa80c8
Hemant <hemantbbadhe@gmail.com>
2019-04-15 00:12:25 +0530

    navbar added

--- welcome/urls.py
+++ welcome/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('sign_up/', views.sign_up, name='sign_up'),
     path('', views.user_login, name='user_login'),
+    path('user_logout/', views.user_logout, name='user_logout'),
     # path('user_login/', views.user_login, name='user_login'),
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -2,7 +2,7 @@ import subprocess
 
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
@@ -24,15 +24,10 @@ def home(request):
 
 def sign_up(request):
     if request.method == "POST":
-
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        # contact_no = request.POST.get('contact_no')
-        # gender = request.POST.get('gender')
-        # address = request.POST.get('address')
-
         user_obj = User.objects.create(first_name=first_name, last_name=last_name, username=email, email=email)
         user_obj.set_password(password)
         user_obj.save()
@@ -42,7 +37,7 @@ def sign_up(request):
         # Generate Private key
         name = "{}_{}".format(user_obj.first_name, user_obj.last_name)
         key = ECC.generate(curve='P-256')
-        f = open('private_keys/' + name + '.pem', 'wt')
+        f = open('utils/private_' + name + '.pem', 'wt')
         f.write(key.export_key(format='PEM'))
         f.close()
 
@@ -51,17 +46,17 @@ def sign_up(request):
         print('**********************')
 
         # Generate Public Key
-        os.system("openssl ec -in private_keys/" + name + ".pem -pubout -out public_keys/" + name + ".pem")
+        os.system("openssl ec -in utils/private_" + name + ".pem -pubout -out utils/public_" + name + ".pem")
 
-        temp = open("public_keys/" + name + ".pem", "r")
+        temp = open("utils/public_" + name + ".pem", "r")
         user_details_obj.public_key = temp.read()
         user_details_obj.save()
-
         # Delete private key after generating public key
-        try:
-            os.remove('private_keys/' + name + '.pem')
-        except Exception as e:
-            pass
+        # try:
+        #     os.remove('utils/private_' + name + '.pem')
+        #     os.remove('utils/public_' + name + '.pem')
+        # except Exception as e:
+        #     pass
         messages.success(request, 'User Sign Up success..!')
         return redirect('/')
 
@@ -77,7 +72,6 @@ def user_login(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                # return HttpResponse("Login success")
                 return redirect('/home')
             else:
                 return HttpResponse("Login Failed")
@@ -85,3 +79,9 @@ def user_login(request):
             return HttpResponse("Invalid login")
     elif request.method == "GET":
         return render(request, 'welcome/login.html')
+
+
+@login_required(login_url='/')
+def user_logout(request):
+    logout(request)
+    return redirect('/')
